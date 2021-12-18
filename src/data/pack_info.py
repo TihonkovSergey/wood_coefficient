4324df31a2a1220bcc7ee33b6c3761fd87385807
TihonkovSergey <slezki.edinoroga@mail.ru>
2021-12-18 19:18:36 +0300

    minor fixes in get_packs

--- src/data/pack_info.py
+++ src/data/pack_info.py
@@ -2,13 +2,14 @@ import pandas as pd
 from definitions import DATA_DIR
 
 
-def get_packs():
+def get_packs() -> pd.DataFrame:
     table_nek = pd.read_csv(DATA_DIR.joinpath('table_nek_2020_01.csv'))
     pack_counts = table_nek.groupby('path').count().pack_id.reset_index()
-    pack_counts['path'] = pack_counts['path'].apply(lambda x: x.split('/')[-1])
+    pack_counts['path'] = pack_counts['path'].apply(lambda x: "/".join(x.split('/')[-2:]))
     pack_counts.rename(columns={'pack_id': 'packs_count'}, inplace=True)
     return pack_counts
 
 
 if __name__ == '__main__':
-    print(get_packs())
+    pass
+    # print(get_packs())
